04e1b31af150240c21b99172f701273ed6c318d9
mayalacs <ayala.matias.ma@gmail.com>
2023-05-25 23:48:48 -0300

    feat: se modifico el consultaFiltro para los nuevos metodos de log y maper

--- TiendaServicio.api.Autor/aplicacion/ConsultaFiltro.cs
+++ TiendaServicio.api.Autor/aplicacion/ConsultaFiltro.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using TiendaServicio.api.Autor.helper;
 using TiendaServicio.api.Autor.modelo;
 using TiendaServicio.api.Autor.persistencia;
 
@@ -21,23 +22,33 @@ namespace TiendaServicio.api.Autor.aplicacion
         {
 
             private readonly DbContextAutor _contexto;
-            private readonly IMapper _mapper;
+            private readonly IMapperCustom _mapper;
+            private readonly ILoggin _logger;
 
-            public Manejador(DbContextAutor contexto, IMapper mapper)
+            public Manejador(DbContextAutor contexto, IMapperCustom mapper, ILoggin logger)
             {
                 _contexto = contexto;
                 _mapper = mapper;
+                _logger = logger;
             }
-
 
             public async Task<AutorDTO> Handle(AutorUnico request, CancellationToken cancellationToken)
             {
-                var autor = await _contexto.AutorLibro.Where(x => x.AutorLibroGuid == request.AutorGuid).FirstOrDefaultAsync();
-                if (autor == null)
+                AutorDTO autorDTO = null;
+                _logger.Info($"Inicio la busqueda del Autor {request.AutorGuid}");
+                try
                 {
-                    throw new Exception("No se encontro el autor");
+                    var autor = await _contexto.AutorLibro.Where(x => x.AutorLibroGuid == request.AutorGuid).FirstOrDefaultAsync();
+                    if (autor == null)
+                    {
+                        throw new Exception("No se encontro el autor");
+                    }
+                    autorDTO = _mapper.Map<AutorLibro, AutorDTO>(autor);
                 }
-                var autorDTO = _mapper.Map<AutorLibro, AutorDTO>(autor);
+                catch (Exception ex)
+                {
+                    _logger.Error($"Error en la busqueda del Autor {request.AutorGuid} message: {ex.Message}", ex);
+                }
                 return autorDTO;
             }
         }
